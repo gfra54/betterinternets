c65fc99f2f07184b4fe422d78258d9c51b3073f1
Gilles François <gilles@sofoot.com>
2016-09-01 10:31:51 +0200

    favicon

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -12,6 +12,14 @@
 	<?php mts_meta(); ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="/manifest.json">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff6c54">
+	<meta name="theme-color" content="#ff6c54">
+
 </head>
 <body <?php body_class('main'); ?> itemscope itemtype="http://schema.org/WebPage">       
 	<div class="main-container">
